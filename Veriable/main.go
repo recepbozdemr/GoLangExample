f357679b940f4262c7499e8f52c84f64ba5be93d
Recep Bozdemir <48210122+recepbozdemr@users.noreply.github.com>
2022-04-28 14:44:06 +0300

    Go

---
+++ Veriable/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var message string = "Hello World"
+	fmt.Println(message)
+
+	var number int = 77
+	fmt.Println(number)
+
+	var numberFloat float32 = 3.14
+	fmt.Println(numberFloat)
+
+	var IsNull bool = true
+	fmt.Println(IsNull) //Yorum Satırı
+
+	var x, y, z int = 3, 4, 5
+	fmt.Println(x, y, z)
+
+	var name = "Recep"
+	fmt.Println(name)
+}
